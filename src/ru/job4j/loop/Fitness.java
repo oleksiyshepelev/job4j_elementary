0eb46842be27bbbe0ec712bf6103758cf690ff53
Oleksiy Shepelev <oshepelev.git@gmail.com>
2021-01-09 23:23:24 +0200

    5.4. Протеиновая диета [#437553]2.0

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -3,7 +3,7 @@ package ru.job4j.loop;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
-        while (ivan < nik) {
+        while (ivan <= nik) {
             ivan = ivan * 3;
             nik = nik * 2;
             month++;
