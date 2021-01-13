2f72e06dc4a089811f3290c1b353aa40626f39be
Oleksiy Shepelev <oshepelev.git@gmail.com>
2021-01-13 21:56:58 +0200

    6.0.2. Заполнение массива. [#437477]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,9 +5,18 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Petr Petrov";
+        names[1] = "Ivan Ivanov";
+        names[2] = "Dmitriy Dmitriev";
+        names[3] = "Vasiliy Vasiliev";
 
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
